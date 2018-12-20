00937dbaf741ae866d72be8417b72416854e83c6
May09jjo <may55jiron@gmail.com>
2018-12-20 16:33:59 -0500

    Configuraciones para subir proyecto, login deploy.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-/* firebase */
-import { AngularFireModule} from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { environment } from '../environments/environment';
-
-/* imports sb */
 import { LayoutModule } from '@angular/cdk/layout';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { NgModule } from '@angular/core';
 import {
     MatButtonModule,
     MatIconModule,
@@ -20,43 +8,34 @@ import {
     MatSidenavModule,
     MatToolbarModule
 } from '@angular/material';
-
-
-
-/* idiomas */
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './layout/dashboard/dashboard.component';
-import { LayoutComponent } from './layout/layout.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// AoT requires an exported function for factories
 export const createTranslateLoader = (http: HttpClient) => {
-  /* for development
-  return new TranslateHttpLoader(
-      http,
-      '/start-javascript/sb-admin-material/master/dist/assets/i18n/',
-      '.json'
-  );*/
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+    /* for development
+    return new TranslateHttpLoader(
+        http,
+        '/start-javascript/sb-admin-material/master/dist/assets/i18n/',
+        '.json'
+    );*/
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 };
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    DashboardComponent,
-    LayoutComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    OverlayModule,
-    HttpClientModule,
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        OverlayModule,
+        HttpClientModule,
         TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
@@ -64,8 +43,8 @@ export const createTranslateLoader = (http: HttpClient) => {
                 deps: [HttpClient]
             }
         })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
@@ -13,6 +13,7 @@ import { LoginComponent } from './login.component';
         MatInputModule,
         MatCheckboxModule,
         MatButtonModule,
+        MatFormFieldModule,
         FlexLayoutModule.withConfig({addFlexToParent: false})
     ],
     declarations: [LoginComponent]
